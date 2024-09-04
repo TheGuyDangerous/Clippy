f9ca8d273ccd2115b5300e027cf14c109fccfc5d
TheGuyDangerous <TheGuyDangerous@users.noreply.github.com>
2024-09-04 19:39:44 +0000

    deploy: dd4b6c1d4d0217b55b65f61c4daed2351754dbfd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"main.dart.js": "b602a9fb315da10b0928504cef5104d8",
-"index.html": "c3b015085714eaa7cb938c42ceb02aaf",
-"/": "c3b015085714eaa7cb938c42ceb02aaf",
+"index.html": "a69b284b94cddbe31b8dda4af249eb5e",
+"/": "a69b284b94cddbe31b8dda4af249eb5e",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
